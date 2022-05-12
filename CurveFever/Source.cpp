336a2f14796dc87e7b000d2edbf07bd8ddd137dd
kriskros <krzysztofczuba884@gmail.com>
2022-05-12 14:51:12 +0200

    client-server

--- CurveFever/Source.cpp
+++ CurveFever/Source.cpp
@@ -5,7 +5,8 @@
 #include <csignal>
 #include <map>
 #include <thread>
-
+#include <mutex>
+#include <chrono>
 #include "imgui/imgui.h"
 #include "imgui/imgui-SFML.h"
 
@@ -107,6 +108,10 @@ public:
 	void applyDisplacement(float distance, float angle) {
 		current += {distance * std::cos(angle), distance * std::sin(angle)};
 	}
+	void setPosition(sf::Vector2f& n) {
+		previous = current;
+		current = n;
+	}
 	void applyDisplacement(Vector& v) {
 		previous = current;
 		current = current + v.getDisplacement();
@@ -146,7 +151,17 @@ public:
 	}
 };
 
+enum class Inputs {
+	Left = 1,
+	Right = 2,
+	Space = 3,
+};
+
+
 class ControlManager {
+public:
+	std::string type;
+	std::map<Inputs, bool> inputMap;
 	/*
 		How each player accesses data about their movement.
 
@@ -154,30 +169,72 @@ class ControlManager {
 		Keyboard would recieve keyboard updates from a seperate thread
 
 	*/
+	virtual void poll() = 0;
 };
 
 class NetworkControl : public ControlManager {
-
+	sf::TcpSocket& socket;
+public:
+	NetworkControl(sf::TcpSocket& s) : socket(s) {
+		type = "network";
+	}
+	void poll() {
+		
+	}
+	sf::TcpSocket& getUnderlyingSocket() {
+		return socket;
+	}
 };
 
 //OPTIONAL
 class AIControl: public ControlManager {
-
 };
-
+/*
 class KeyboardControl: public ControlManager {
-
+	std::map<sf::Keyboard::Key, Inputs> translations;
+	std::mutex keyboardMutex;
+	bool isPolling = false;
+	KeyboardControl() {
+		type = "keyboard";
+		translations = {
+			{sf::Keyboard::D, Inputs::Right},
+			{sf::Keyboard::A, Inputs::Left},
+			{sf::Keyboard::Space, Inputs::Space},
+		};
+		isPolling = true;
+	}
+	void poll(sf::Event& event) {
+		switch (event.type) {
+		case(sf::Event::KeyPressed): {
+			keyboardMutex.lock();
+			inputMap[translations[event.key.code]] = true;
+			keyboardMutex.unlock();
+			break;
+		};
+		case(sf::Event::KeyReleased): {
+			keyboardMutex.lock();
+			inputMap[translations[event.key.code]] = false;
+			keyboardMutex.unlock();
+			break;
+		}
+		}
+	}
 };
+*/
 
 class Player : public LineManager, public PositionManager {
 	bool placesPath = true;
 	int size{};
+	int id;
 public:
 	friend class MyRenderWindow;
-	Player(sf::Vector2f p, int s) : PositionManager(p), LineManager() {
+	Player(sf::Vector2f p = { 400, 400 }, int s = 5) : PositionManager(p), LineManager() {
 		size = s;
 		placesPath = true;
 		initiateLine();
+	}
+	int getId() {
+		return id;
 	}
 
 	void moveBy(float distance) {
@@ -268,6 +325,23 @@ public:
 			}
 		}
 	}
+};
+
+class ControlSignals {
+public:
+	bool left, right, space;
+	ControlSignals(bool l = false, bool r = false, bool s = false) {
+		left = l;
+		right = r;
+		space = s;
+	}
+};
+
+class NetworkPlayer : public Player {
+public:
+	sf::TcpSocket*& c;
+	ControlSignals ctrl;
+	NetworkPlayer(sf::TcpSocket*&co) : Player(), c(co), ctrl() {};
 };
 
 class MyRenderWindow : public sf::RenderWindow {
@@ -437,6 +511,7 @@ enum class State {
 	singleplayer = 1,
 	multiplayerMenu,
 	menu,
+	multiplayer
 };
 
 
@@ -464,6 +539,12 @@ void clearBuff(char* buffer) {
 	}
 }
 
+enum class networkFlags {
+	JOIN = 1,
+	DISCONNECT = 2,
+	TEST = 3,
+};
+
 class networkClient {
 	sf::IpAddress ip;
 	char buffer[200]{};
@@ -473,6 +554,7 @@ class networkClient {
 	sf::TcpSocket socket;
 	const char* w = "welcome";
 public:
+	std::map<sf::Keyboard::Key, bool> keymap;
 	networkClient() {
 		ip = sf::IpAddress::getLocalAddress();
 	}
@@ -505,18 +587,103 @@ public:
 		isConnected = false;
 	}
 	void test() {
-		char testString[] = "test!";
-		cout << "g" << endl;
-		socket.send(testString, strlen(testString));
+		sf::Packet p;
+		std::string t = "TEST";
+		p << t;
+		socket.send(p);
 	}
-
+	void join() {
+		sf::Packet p;
+		std::string t = "JOIN";
+		p << t;
+		socket.send(p);
+	}
+	//works on second attempt
+	bool start() {
+		sf::Packet p;
+		std::string t = "START";
+		p << t;
+		socket.send(p);
+		sf::Packet p2;
+		socket.receive(p2);
+		p2 >> t;
+		if (t == "OK") {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	sf::TcpSocket& getSocket() {
+		return socket;
+	}
 };
 
+void multiplayer(MyRenderWindow& window, State& s, networkClient& net) {
+	Player player;
+	std::thread sendLoopThread([&]() {
+		while (true) {
+			sf::Packet pac1;
+			net.getSocket().receive(pac1);
+			float newX = 0, newY = 0;
+			pac1 >> newX >> newY;
+			sf::Vector2f n;
+			n.x = newX;
+			n.y = newY;
+			player.setPosition(n);
+		}
+		
+		});
+	std::thread recvLoopThread([&]() {
+		while (true) {
+			sf::Packet pac2;
+			bool right = net.keymap[sf::Keyboard::A], left = net.keymap[sf::Keyboard::D], space = net.keymap[sf::Keyboard::Space];
+			std::string procedure = "UPDATE";
+			pac2 << procedure << right << left << space;
+
+			std::cout << "UPDATE" << right << left << space << std::endl;
+			net.getSocket().send(pac2);
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		});
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case(sf::Event::Closed): {
+				window.close();
+			}
+			case(sf::Event::KeyPressed): {
+				net.keymap[event.key.code] = true;
+				break;
+			};
+			case(sf::Event::KeyReleased): {
+				net.keymap[event.key.code] = false;
+				break;
+			}
+			}
+		}
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
+
+}
 void multiplayerMenu(MyRenderWindow& window, State& s, networkClient& net) {
 	if (ImGui::Button("back")) {
 		s = State::menu;
 		net.disconnect();
 	}
+	if (ImGui::Button("start")) {
+		if (net.start()) {
+			s = State::multiplayer;
+		};
+	}
+
+	if (ImGui::Button("join")) {
+		net.join();
+	}
+
 	if (ImGui::Button("test")) {
 		net.test();
 	}
@@ -539,12 +706,142 @@ void multiplayerConnecting(MyRenderWindow& window, State& s, networkClient& net)
 	window.display();
 }
 sf::Mutex globalMutex;
-void server() {
+
+class Game {
+	bool inProgress = false;
+public:
+	std::vector<NetworkPlayer*> players;
+	void start() {
+		for (std::vector<NetworkPlayer*>::iterator p = players.begin(); p != players.end(); ++p) {
+			NetworkPlayer* playerInstance = *p;
+			sf::Packet pac;
+			std::string msg = "OK";
+			pac << msg;
+			std::cout << msg << std::endl;
+			playerInstance->c->send(pac);
+		}
+		inProgress = true;
+		runGameLoop();
+		//rungameloop thread
+	}
+	void runGameLoop() {
+		sf::Time elapsed;
+		sf::Clock mainClock;
+		while (inProgress && players.size() > 0) {
+			for (std::vector<NetworkPlayer*>::iterator p = players.begin(); p != players.end(); ++p) {
+				NetworkPlayer* PlayerInstance = *p;
+				std::cout << PlayerInstance->ctrl.left << PlayerInstance->ctrl.right << std::endl;
+				PlayerInstance->changeAngle(PlayerInstance->ctrl.left * -0.00001 * elapsed.asMicroseconds());
+				PlayerInstance->changeAngle(PlayerInstance->ctrl.right * 0.00001 * elapsed.asMicroseconds());
+				PlayerInstance->moveBy(0.00003 * elapsed.asMicroseconds());
+				if (PlayerInstance->checkForCollision()) {
+					std::cout << "PLAYER COLLIDED" << std::endl;
+				}
+			}
+			sendUpdates();
+			elapsed = mainClock.restart();
+		}
+	}
+	void sendUpdates() {
+		for (std::vector<NetworkPlayer*>::iterator p = players.begin(); p != players.end(); ++p) {
+			NetworkPlayer*& PlayerInstance = *p;
+			sf::Packet pac;
+			sf::Vector2f newPosition = PlayerInstance->getPosition();
+			pac << newPosition.x << newPosition.y;
+			PlayerInstance->c->send(pac);
+		}
+
+	}
+	void update() {
+		
+	}
+};
+
+class Server {
+public:
 	sf::TcpListener listener;
 	std::size_t recsize{};
 	sf::SocketSelector selector;
 	std::vector<sf::TcpSocket*> sockets;
-	std::thread t([&]() {
+	std::vector<Game*> games;
+	void parseRecieved(sf::Packet p, sf::TcpSocket*& s) {
+		std::string st;
+		p >> st;
+		std::cout << st << std::endl;
+		if (st == "TEST") {
+			std::cout << st << std::endl;
+			std::cout << games[0]->players.size() << std::endl;
+		}
+		if (st == "START") {
+			std::thread* t = new std::thread([&]() {games[0]->start();});
+		}
+		if (st == "UPDATE") {
+			bool isFound = false;
+			NetworkPlayer* savedPlayer = 0;
+			for (std::vector<Game*>::iterator g = games.begin(); g != games.end(); ++g) {
+				Game*& f = *g; // I think only the last *& matters since these all are pointers, but whatever
+				for (std::vector<NetworkPlayer*>::iterator p = f->players.begin(); p != f->players.end(); ++p) {
+					NetworkPlayer*& o = *p;
+					sf::TcpSocket*& playerSocket = o->c;
+					if (playerSocket == s) {
+						isFound = true;
+						savedPlayer = o;
+						break;
+					}
+				}
+				if (isFound) {
+					break;
+				}
+			}
+			if (isFound) {
+				bool left = false;
+				bool right = false;
+				bool space = false;
+				p >> left >> right >> space;
+				std::cout << "UPDATING CONTROLS ON " << s->getRemotePort() << ": " << left << right << space << std::endl;
+				savedPlayer->ctrl = ControlSignals(left, right, space);
+			}
+		}
+		if (st == "JOIN") {
+			bool isFound = false;
+			NetworkPlayer* savedPlayer = 0;
+			for (std::vector<Game*>::iterator g = games.begin(); g != games.end(); ++g) {
+				Game*& f = *g;
+				for (std::vector<NetworkPlayer*>::iterator p = f->players.begin(); p != f->players.end(); ++p) {
+					NetworkPlayer*& o = *p;
+					sf::TcpSocket*& playerSocket = o->c;
+					if (playerSocket == s) {
+						isFound = true;
+						savedPlayer = o;
+						break;
+					}
+				}
+				if (isFound) {
+					break;
+				}
+			}
+
+
+			if (isFound) {
+				std::cout << "PLAYER ALREADY EXISTS FROM " << s->getRemoteAddress() << ":" << s->getRemotePort() << std::endl;
+			}
+			else {
+				NetworkPlayer* playerInstance = new NetworkPlayer(s);
+				games[0]->players.push_back(playerInstance);
+				sf::Packet ps;
+				std::cout << "PLAYER CREATED FROM " << s->getRemoteAddress() << ":" << s->getRemotePort() << std::endl;
+				s->send(ps);
+			}
+		}
+	};
+	void start() {
+		games.push_back(new Game);
+		std::thread t([&]() {acceptLoop();});
+		std::thread y([&]() {recvLoop();});
+		t.join();
+		y.join();
+	};
+	void acceptLoop() {
 		while (true) {
 			if (listener.listen(53000) != sf::Socket::Done) {
 				std::cout << "JD" << std::endl;
@@ -560,18 +857,16 @@ void server() {
 			selector.add(*client);
 			std::cout << "client accepted" << endl;
 		}
-		});
-/*whenever a user enters there is a huge lag*/
-
-	std::thread f([&]() {
+	};
+	void recvLoop()
+	{
 		while (true) {
-
-			cout << "jd" << endl;			
 			if (selector.wait(sf::seconds(10.f))) {
 				for (auto a = sockets.begin(); a != sockets.end();) {
 					if (selector.isReady(**a)) {
 						char buff[200]{};
-						sf::Socket::Status s = (*a)->receive(buff, sizeof(buff), recsize);
+						sf::Packet p;
+						sf::Socket::Status s = (*a)->receive(p);
 						if (s == sf::Socket::Status::Disconnected)
 						{
 							selector.remove(**a);
@@ -580,12 +875,9 @@ void server() {
 							//delete *a;
 							std::cout << sockets.size() << std::endl;
 						}	else {
+							parseRecieved(p, *a);
 							a++;
 						};
-
-						cout << buff << endl;
-							
-						clearBuff(buff);
 					}
 					else {
 						a++;
@@ -596,9 +888,13 @@ void server() {
 				std::cout << "timeout" << std::endl;
 			}
 		}
-	});
-	t.join();
-	f.join();
+	};
+};
+
+void server() {
+/*whenever a user enters there is a huge lag*/
+	Server s;
+	s.start();
 }
 
 
@@ -635,6 +931,10 @@ void client() {
 			singleplayer(window);
 			break;
 		}
+		case State::multiplayer: {
+			multiplayer(window, currentState, net);
+			break;
+		}
 		case State::multiplayerMenu: {
 			if (!net.isWorking()) {
 				net.connect();
@@ -654,7 +954,7 @@ void client() {
 
 int main(int argc, char* argv[]) {
 	// initiate window and globally used values
-	bool testServer = true;
+	bool testServer = false;
 	if (argc > 1) {
 		if ((int)(char)argv[1][0] == (int)'h') {
 			testServer = !testServer;
